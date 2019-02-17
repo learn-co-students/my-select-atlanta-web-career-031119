3a670f75851536225405360405e844f0b82c9aa1
active-object-6703 <nerajno@gmail.com>
2019-02-17 03:53:03 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+  # your code here!
+ arr = []
+ i = 0
+ # num = collection[i]
+ while i < collection.length
+   collection[i] % 2 == 0
+   arr.push(collection[i])
+ i+=1
+ end
+ # arr
 end
