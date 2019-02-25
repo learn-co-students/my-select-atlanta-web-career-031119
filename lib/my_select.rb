0e5efafe8b59ea62c10c9223f521c12c7f89d48a
github username <github email address>
2019-02-25 11:49:52 -0500

    Done

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
-end
+  nu_array = []
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    collection.each {|num|
+      if num.even? == true
+      nu_array << num
+        i += 1
+      end}
+      return nu_array
+    end
+  end
