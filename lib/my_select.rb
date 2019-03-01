c2e328f5f10c871eda21fdc6876e90a7fd76f94f
Tyler Hueter <tylerhueter08@gmail.com>
2019-03-01 00:46:07 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+ block_given?
+ x = 0 
+ new_array = []
+ while x < collection.length 
+ if yield(collection[x]) == true 
+   new_array.push(collection[x])
+ end
+   x += 1 
+  
+ end 
+ new_array
 end
