e1be11de64dac1bdcfb516854766023519d5de78
Travis Martin <travisemerymartin@gmail.com>
2019-03-04 18:27:15 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,20 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    if collection.length>0
+      i=0
+      new_array=[]
+      while i<collection.length
+        if collection[i] === yield collection[i]
+          new_array<<collection[i]
+          i+=1
+        else
+          i+=1
+        end
+        return new_array
+    else
+      puts "Collection contains 0 entries"
+    end
+  else
+    return collection
+  end
 end
